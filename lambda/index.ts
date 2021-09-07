626e80bf21c7ff069d474b35403dbac334ae25c2
semyon <schweppes2205@gmail.com>
2021-09-07 02:43:12 -0700

    6 dynamodb empty objects creation code added. Issues: tsc creates unexpectedly huge js code. tsc watch creates js code as expected

--- lambda/index.ts
+++
@@ -1,7 +0,0 @@
-import * as fetch from 'node-fetch';
-
-export const handler = async function customFunc() {
-    var b = await fetch.default('https://swapi.dev/api/people');
-    var c = await b.json();
-    console.log(c);
-}
--- lib/epam_diploma-stack.ts
+++ lib/epam_diploma-stack.ts
@@ -1,14 +1,31 @@
-import * as lambda from '@aws-cdk/aws-lambda'
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
-
+import * as dynamo from '@aws-cdk/aws-dynamodb';
 export class EpamDiplomaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    let sharedTalbeProp: dynamo.TableProps = {
+      partitionKey: {
+        name: 'id',
+        type: dynamo.AttributeType.STRING
+      },
+      readCapacity: 2,
+      writeCapacity: 2,
+    };
+    const dynamoResourcePeople = new dynamo.Table(this, 'dynamoResourcePeople', sharedTalbeProp);
+    const dynamoResourceFilms = new dynamo.Table(this, 'dynamoResourceFilms', sharedTalbeProp);
+    const dynamoResourcePlanets = new dynamo.Table(this, 'dynamoResourcePlanets', sharedTalbeProp);
+    const dynamoResourceSpecies = new dynamo.Table(this, 'dynamoResourceSpecies', sharedTalbeProp);
+    const dynamoResourceStarships = new dynamo.Table(this, 'dynamoResourceStarships', sharedTalbeProp);
+    const dynamoResourceVehicles = new dynamo.Table(this, 'dynamoResourceVehicles', sharedTalbeProp);
 
-    const fn = new lambda.Function(this, 'MyFunction', {
+    const fillResourcesTables = new lambda.Function(this, 'fillResourcesTables', {
       runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'index.handler',
+      handler: 'fillResourcesTables.handler',
       code: lambda.Code.fromAsset('./lambda'),
+      timeout: cdk.Duration.seconds(10),
     });
+
+
   }
 }
