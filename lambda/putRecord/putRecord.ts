c621f6b983d8326fde24a209637b8489cb56ff98
semyon <schweppes2205@gmail.com>
2021-11-16 22:27:14 +0300

    fix/CORS: add CORS enabling into API GW construct. Fix put method to read body. Add CORS headers to put responce helper

--- lambda/putRecord/putRecord.ts
+++ lambda/putRecord/putRecord.ts
@@ -1,21 +1,21 @@
 import * as AWS from 'aws-sdk';
 import { RestApiResponse } from "./helper/restApiResponse";
 class QueryParamHelper {
-    constructor(public resource: string, public record: string) { }
+    constructor(public resource: string, public record: any) { }
 }
 
 export const handler = async (event: any) => {
     console.log(JSON.stringify(event));
-    if (!event.queryStringParameters) { return new RestApiResponse("400", "No parameters are mentioned") };
+    if (!event.body) { return new RestApiResponse("400", "No parameters are mentioned") };
     // all user parameters came from the API Request
-    const restPutParams: QueryParamHelper = event.queryStringParameters;
+    const restPutParams: QueryParamHelper = JSON.parse(event.body);
     const ddbAgent = new AWS.DynamoDB.DocumentClient();
     // check essential params
     if (restPutParams.resource && restPutParams.record) {
         try {
             let ddbQueryParams: AWS.DynamoDB.PutItemInput = {
                 TableName: restPutParams.resource,
-                Item: JSON.parse(restPutParams.record),
+                Item: restPutParams.record,
             };
             let originalResponce = await ddbAgent.put(ddbQueryParams).promise();
             console.log(`record created. params: ${JSON.stringify(ddbQueryParams)}`);
